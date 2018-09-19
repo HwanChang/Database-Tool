d9d3de7f09cd3c79a83e1642a9f81a021b1087ce
HwanChang <32251594+HwanChang@users.noreply.github.com>
2018-09-19 13:42:36 +0900

    Generic Excel File

---
+++ Database Tool/MainFrame.py
@@ -0,0 +1,137 @@
+from Tkinter import *
+import Tkconstants, tkFileDialog, ttk, openpyxl, Oracle_Tibero
+
+class MainFrame(Frame) :
+	def __init__(self, master) :
+		Frame.__init__(self, master)
+		self.master = master
+		self.master.title('Database Tool')
+		self.pack(fill = BOTH, expand = True)
+
+	# Select DBMS.
+		frame1 = Frame(self)
+		frame1.pack(fill = X)
+
+		labelDBMS = Label(frame1, text = 'DBMS', width = 10)
+		labelDBMS.pack(side = LEFT, padx = 10, pady = 10)
+		self.comboDBMS = ttk.Combobox(frame1, width = 20)
+		self.comboDBMS['values'] = ('Oracle / Tibero', 'Altibase', 'MS-SQL', 'MySQL / MariaDB')
+		self.comboDBMS.current(0)
+		self.comboDBMS.pack(side = LEFT, pady = 10)
+
+	# Connection info.
+		frame2 = Frame(self)
+		frame2.pack(fill = X)
+
+		labelAddr = Label(frame2, text = 'IP', width = 5)
+		labelAddr.pack(side = LEFT, padx = 10, pady = 10)
+		self.entryAddr = ttk.Entry(frame2)
+		self.entryAddr.pack(side = LEFT, expand = False)
+
+		labelPort = Label(frame2, text = 'Port', width = 5)
+		labelPort.pack(side = LEFT, padx = 10, pady = 10)
+		self.entryPort = ttk.Entry(frame2)
+		self.entryPort.pack(side = LEFT, expand = False)
+
+		labelSid = Label(frame2, text = 'sid', width = 5)
+		labelSid.pack(side = LEFT, padx = 10, pady = 10)
+		self.entrySid = ttk.Entry(frame2)
+		self.entrySid.pack(side = LEFT, expand = False)
+
+	# User info.
+		frame3 = Frame(self)
+		frame3.pack(fill = X)
+
+		labelID = Label(frame3, text = 'ID', width = 5)
+		labelID.pack(side = LEFT, padx = 10, pady = 10)
+		self.entryID = ttk.Entry(frame3)
+		self.entryID.pack(side = LEFT, expand = False)
+
+		labelPW = Label(frame3, text = 'PW', width = 5)
+		labelPW.pack(side = LEFT, padx = 10, pady = 10)
+		self.entryPW = ttk.Entry(frame3)
+		self.entryPW.pack(side = LEFT, expand = False)
+
+	# File path.
+		frame4 = Frame(self)
+		frame4.pack(fill = X, pady = 10)
+
+		self.pathStr = StringVar()
+
+		labelPath = Label(frame4, text = 'Path', width = 5)
+		labelPath.pack(side = LEFT, padx = 5)
+		self.entryPath = ttk.Entry(frame4, textvariable = self.pathStr)
+		self.entryPath.pack(side = LEFT, fill = X, padx = 5, expand = True)
+		buttonPath = ttk.Button(frame4, text = 'open', command = self.openFunction)
+		buttonPath.pack(side = LEFT, padx = 20)
+	# Select sheet.
+		frame5 = Frame(self)
+		frame5.pack(fill = X, padx = 60)
+
+		self.comboSheet = ttk.Combobox(frame5, width = 20)
+		self.comboSheet['values'] = ('all')
+		self.comboSheet.current(0)
+		self.comboSheet.pack(side = RIGHT, pady = 10)
+		labelSheet = Label(frame5, text = 'Sheet', wid = 5)
+		labelSheet.pack(side = RIGHT, padx = 5)
+
+	# Function button.
+		frame6 = Frame(self)
+		frame6.pack(fill = X, padx = 10, pady = 10)
+
+		buttonED = ttk.Button(frame6, text = 'Excel -> DB Scheme', width = 40, command = self.clickED)
+		buttonES = ttk.Button(frame6, text = 'Excel -> SQL File', width = 40, command = self.clickES)
+		buttonDE = ttk.Button(frame6, text = 'DB Scheme -> Excel', width = 40, command = self.clickDE)
+		buttonDS = ttk.Button(frame6, text = 'DB Scheme -> SQL File', width = 40, command = self.clickDS)
+
+		buttonED.grid(row = 0, column = 0, padx = 10, pady = 10)
+		buttonES.grid(row = 0, column = 1, padx = 10, pady = 10)
+		buttonDE.grid(row = 1, column = 0, padx = 10, pady = 10)
+		buttonDS.grid(row = 1, column = 1, padx = 10, pady = 10)
+
+	# State of progress.
+		frame7 = Frame(self)
+		frame7.pack(fill = X, padx = 10, pady = 10)
+
+		scrollbar = Scrollbar(frame7)
+		scrollbar.pack(side = RIGHT, fill = Y)
+		self.textB = Text(frame7)
+		self.textB.pack(fill = BOTH, expand = 1)
+		self.textB.config(yscrollcommand = scrollbar.set)
+		scrollbar.config(command = self.textB.yview)
+
+# MainFrame excel file open.
+	def openFunction(self) :
+		self.pathStr.set(tkFileDialog.askopenfilename(initialdir = "/", title = "Select file", filetypes = (("excel files","*.xlsx"), ("sql files", "*.sql"), ("all files", "*.*"))))
+		filePath = self.entryPath.get()
+		if '.xlsx' in filePath :
+			excelFile = openpyxl.load_workbook(filename = filePath)
+			items = ['all'] + excelFile.sheetnames
+			self.comboSheet['values'] = items
+		else :
+			self.comboSheet['values'] = []
+
+# Click events.
+	def clickED(self) :
+		parameter = {'IP' : self.entryAddr.get(), 'Port' : self.entryPort.get(), 'sid' : self.entrySid.get(), 'ID' : self.entryID.get(), 'PW' : self.entryPW.get(), 'Path' : self.entryPath.get(), 'Sheet' : self.comboSheet.get()}
+		if self.comboDBMS.get() == 'Oracle / Tibero' :
+			parameter['Type'] = 'ED'
+			Oracle_Tibero.Oracle_Tibero(parameter, self.textB)
+
+	def clickES(self) :
+		parameter = {'IP' : self.entryAddr.get(), 'Port' : self.entryPort.get(), 'sid' : self.entrySid.get(), 'ID' : self.entryID.get(), 'PW' : self.entryPW.get(), 'Path' : self.entryPath.get(), 'Sheet' : self.comboSheet.get()}
+		if self.comboDBMS.get() == 'Oracle / Tibero' :
+			parameter['Type'] = 'ES'
+			Oracle_Tibero.Oracle_Tibero(parameter, self.textB)
+
+	def clickDE(self) :
+		parameter = {'IP' : self.entryAddr.get(), 'Port' : self.entryPort.get(), 'sid' : self.entrySid.get(), 'ID' : self.entryID.get(), 'PW' : self.entryPW.get(), 'Path' : self.entryPath.get(), 'Sheet' : self.comboSheet.get()}
+		if self.comboDBMS.get() == 'Oracle / Tibero' :
+			parameter['Type'] = 'DE'
+			Oracle_Tibero.Oracle_Tibero(parameter, self.textB)
+
+	def clickDS(self) :
+		parameter = {'IP' : self.entryAddr.get(), 'Port' : self.entryPort.get(), 'sid' : self.entrySid.get(), 'ID' : self.entryID.get(), 'PW' : self.entryPW.get(), 'Path' : self.entryPath.get(), 'Sheet' : self.comboSheet.get()}
+		if self.comboDBMS.get() == 'Oracle / Tibero' :
+			parameter['Type'] = 'DS'
+			Oracle_Tibero.Oracle_Tibero(parameter, self.textB)
---
+++ Database Tool/Oracle_Tibero.py
@@ -0,0 +1,285 @@
+import Tkconstants, tkFileDialog, ttk, openpyxl, cx_Oracle
+from Tkinter import *
+from openpyxl.styles import Font
+
+class Oracle_Tibero :
+	def __init__(self, info, textB) :
+		self.info = info
+		self.textB = textB
+	# Functions by button type.
+		if self.info['Type'] == 'ED' or self.info['Type'] == 'ES' :
+			self.sendSQL = {}
+			self.excel_document = openpyxl.load_workbook(self.info['Path'])
+			self.sheetList = self.excel_document.sheetnames
+			if self.info['Sheet'] == 'all' :
+				for sheetnameList in self.sheetList :
+					self.sendSQL[str(sheetnameList)] = ''
+					self.mainFunction(str(sheetnameList))
+			else :
+				self.mainFunction(self.info['Sheet'])
+
+		elif self.info['Type'] == 'DE' :
+			self.DEmkFunction()
+
+		elif self.info['Type'] == 'DS' :
+			self.DSmkFunction()
+# Main function about excel -> DB Scheme, Excel -> SQL File.
+	def mainFunction(self, name) :
+		self.name = name
+		sheet = self.excel_document[name]
+
+		cl = []
+		kr = []
+		ty = []
+		nu = []
+		ky = []
+		SQL = ''
+		PKEY = ''
+		k = False
+
+		all_rows = sheet.rows
+		for row in all_rows :
+			if row[3].value is not None :
+				cl.append(row[1].value)
+				kr.append(row[2].value)
+				ty.append(row[3].value)
+				nu.append(row[4].value)
+				ky.append(row[5].value)
+
+		tableName = sheet['C4'].value
+		for i in range(1, len(cl)) :
+			if i == len(cl)-1 :
+				if nu[i] == 'N' :
+					SQL += cl[i] + ' ' + ty[i] + ' NOT NULL'
+				else :
+					SQL += cl[i] + ' ' + ty[i]
+
+			else :
+				if nu[i] == 'N' :
+					SQL += cl[i] + ' ' + ty[i] + ' NOT NULL, '
+				else :
+					SQL += cl[i] + ' ' + ty[i] + ', '
+
+			if ky[i] == 'PK' :
+				k = True
+				PKEY += ', CONSTRAINT ' + str(tableName) + 'pk PRIMARY KEY (' + cl[i] + ')'
+		if k == True :
+			self.sendSQL[name] = 'CREATE TABLE ' + str(tableName) + ' ( ' + SQL + PKEY + ' )'
+		else :
+			self.sendSQL[name] = 'CREATE TABLE ' + str(tableName) + ' ( ' + SQL + ' )'
+
+	# Excel -> DB Scheme function
+		if self.info['Type'] == 'ED' :
+			self.textB.delete(1.0, END)
+			dsn = cx_Oracle.makedsn(self.info['IP'], self.info['Port'], self.info['sid'])
+			db = cx_Oracle.connect(self.info['ID'], self.info['PW'], dsn)
+			cursor = db.cursor()
+			cursor.execute(self.sendSQL[name])
+			db.close()
+			self.textB.insert(1.0, 'Excel -> DB Scheme Complete!')
+
+		elif self.info['Type'] == 'ES' :
+			if self.info['Sheet'] == 'all' :
+				if name == self.sheetList[len(self.sheetList)-1] :
+					self.ESmkFunction()
+			else :
+				self.ESmkFunction()
+
+# Excel -> SQL File function
+	def ESmkFunction(self) :
+		self.saveWindow = Toplevel()
+		self.saveWindow.title('SQL File')
+		self.saveWindow.geometry('600x100+200+200')
+		self.saveWindow.resizable(False, False)
+
+	# File save path.
+		frame_save = Frame(self.saveWindow)
+		frame_save.pack(fill = X, padx = 10, pady = 10)
+
+		lablePath_save = Label(frame_save, text = 'Path', width = 5)
+		lablePath_save.pack(side = LEFT, padx = 5)
+		self.entryPath_save = ttk.Entry(frame_save)
+		self.entryPath_save.pack(side = LEFT, fill = X, padx = 5, expand = True)
+		buttonPath_save = ttk.Button(frame_save, text = 'path', command = self.pathESFunction)
+		buttonPath_save.pack(side = LEFT, padx = 5)
+		buttonSave_save = ttk.Button(frame_save, text = 'save', command = self.saveSQLFunction)
+		buttonSave_save.pack(side = RIGHT, padx = 5, pady = 5)
+
+		self.saveWindow.mainloop()
+
+# Excel -> SQL File function
+	def DEmkFunction(self) :
+		self.DEWindow = Toplevel()
+		self.DEWindow.title('Excel File')
+		self.DEWindow.geometry('600x100+200+200')
+		self.DEWindow.resizable(False, False)
+
+		frame_DE = Frame(self.DEWindow)
+		frame_DE.pack(fill = X, padx = 10, pady = 10)
+		DElablePath = Label(frame_DE, text = 'Path', width = 5)
+		DElablePath.pack(side = LEFT, padx = 5)
+		self.DEentryPath = ttk.Entry(frame_DE)
+		self.DEentryPath.pack(side = LEFT, fill = X, padx = 5, expand = True)
+		DEbuttonPath = ttk.Button(frame_DE, text = 'path', command = self.pathDEFunction)
+		DEbuttonPath.pack(side = LEFT, padx = 5)
+
+		frame_DE2 = Frame(self.DEWindow)
+		frame_DE2.pack(fill = X, padx = 10, pady = 5)
+		DEbuttonSave_save = ttk.Button(frame_DE2, text = 'save', command = self.saveExcelFunction)
+		DEbuttonSave_save.pack(side = RIGHT, padx = 5, pady = 5)
+		DEdsn = cx_Oracle.makedsn(self.info['IP'], self.info['Port'], self.info['sid'])
+		self.DEdb = cx_Oracle.connect(self.info['ID'], self.info['PW'], DEdsn)
+		self.cursorDE = self.DEdb.cursor()
+		self.cursorDE.execute('select TABLE_NAME from tabs')
+		tableList = self.cursorDE.fetchall()
+		tbl = []
+		for table in tableList :
+			tbl = tbl + [table[0]]
+		self.comboTbl = ttk.Combobox(frame_DE2, width = 20)
+		self.comboTbl['values'] = tbl
+		self.comboTbl.current(0)
+		self.comboTbl.pack(side = RIGHT, padx = 5, pady = 5)
+		labelSheet = Label(frame_DE2, text = 'Sheet', width = 5)
+		labelSheet.pack(side = LEFT, padx = 5, pady = 5)
+		self.entrySheet = ttk.Entry(frame_DE2, width = 20)
+		self.entrySheet.pack(side = LEFT, padx = 5, pady = 5)
+
+		self.DEWindow.mainloop()
+
+	def DSmkFunction(self) :
+		self.DSWindow = Toplevel()
+		self.DSWindow.title('SQL File')
+		self.DSWindow.geometry('600x100+200+200')
+		self.DSWindow.resizable(False, False)
+
+		frame_DS = Frame(self.DSWindow)
+		frame_DS.pack(fill = X, padx = 10, pady = 10)
+		DSlablePath = Label(frame_DS, text = 'Path', width = 5)
+		DSlablePath.pack(side = LEFT, padx = 5)
+		self.DSentryPath = ttk.Entry(frame_DS)
+		self.DSentryPath.pack(side = LEFT, fill = X, padx = 5, expand = True)
+		DSbuttonPath = ttk.Button(frame_DS, text = 'path', command = self.pathDSFunction)
+		DSbuttonPath.pack(side = LEFT, padx = 5)
+
+		frame_DS2 = Frame(self.DSWindow)
+		frame_DS2.pack(fill = X, padx = 10, pady = 5)
+		DSbuttonSave_save = ttk.Button(frame_DS2, text = 'save', command = self.DB_SQLFunction)
+		DSbuttonSave_save.pack(side = RIGHT, padx = 5, pady = 5)
+		DSdsn = cx_Oracle.makedsn(self.info['IP'], self.info['Port'], self.info['sid'])
+		self.DSdb = cx_Oracle.connect(self.info['ID'], self.info['PW'], DSdsn)
+		self.cursorDS = self.DSdb.cursor()
+		self.cursorDS.execute('select TABLE_NAME from tabs')
+		tableList = self.cursorDS.fetchall()
+		tbl = []
+		for table in tableList :
+			tbl = tbl + [table[0]]
+		self.comboTblDS = ttk.Combobox(frame_DS2, width = 20)
+		self.comboTblDS['values'] = tbl
+		self.comboTblDS.current(0)
+		self.comboTblDS.pack(side = RIGHT, padx = 5, pady = 5)
+
+		self.DSWindow.mainloop()
+
+	def pathESFunction(self) :
+		self.entryPath_save.insert(0, tkFileDialog.asksaveasfilename(initialdir = "/",title = "Select file", filetypes = (("sql files", "*.sql"), ("all files", "*.*"))))
+		self.saveWindow.lift()
+
+	def pathDEFunction(self) :
+		self.DEentryPath.insert(0, tkFileDialog.asksaveasfilename(initialdir = "/",title = "Select file", filetypes = (("excel files","*.xlsx"), ("all files", "*.*"))))
+		self.DEWindow.lift()
+
+	def pathDSFunction(self) :
+		self.DSentryPath.insert(0, tkFileDialog.asksaveasfilename(initialdir = "/",title = "Select file", filetypes = (("sql files", "*.sql"), ("all files", "*.*"))))
+		self.DSWindow.lift()
+
+	def saveSQLFunction(self) :
+		self.textB.delete(1.0, END)
+		f = open(self.entryPath_save.get(), 'w')
+		if self.info['Sheet'] == 'all' :
+			for sheetnamesList in self.sheetList :
+				f.write(self.sendSQL[str(sheetnamesList)] + '\n\n')
+		else :
+			f.write(self.sendSQL[self.name])
+		f.close()
+		self.textB.insert(1.0, 'Excel -> SQL File Complete!\n\n')
+		self.textB.insert(END, self.entryPath_save.get())
+		self.saveWindow.destroy()
+
+	def saveExcelFunction(self) :
+		self.textB.delete(1.0, END)
+		self.cursorDE.execute("select COLUMN_NAME, DATA_TYPE, DATA_LENGTH from USER_TAB_COLUMNS where TABLE_NAME = '" + self.comboTbl.get() +"'")
+		saveList = self.cursorDE.fetchall()
+
+		wb = openpyxl.Workbook()
+		sheetmkNew = wb.active
+		sheetmkNew.title = self.entrySheet.get()
+
+		sheetmkNew.column_dimensions['A'].width = 25
+		sheetmkNew.column_dimensions['B'].width = 15
+		sheetmkNew.column_dimensions['C'].width = 15
+		sheetmkNew.freeze_panes = 'A4'
+
+		fontObj = Font(size = 20, bold = True)
+		fontBold = Font(bold = True)
+		sheetmkNew['A1'].font = fontObj
+		sheetmkNew['A3'].font = fontBold
+		sheetmkNew['B3'].font = fontBold
+		sheetmkNew['C3'].font = fontBold
+
+		cnt = 4
+		sheetmkNew.cell(row = 1, column = 1).value = self.comboTbl.get()
+		sheetmkNew.cell(row = 3, column = 1).value = 'COLUMN_NAME'
+		sheetmkNew.cell(row = 3, column = 2).value = 'DATA_TYPE'
+		sheetmkNew.cell(row = 3, column = 3).value = 'DATA_LENGTH'
+		for into in saveList :
+			sheetmkNew.cell(row = cnt, column = 1).value = into[0]
+			sheetmkNew.cell(row = cnt, column = 2).value = into[1]
+			sheetmkNew.cell(row = cnt, column = 3).value = into[2]
+			cnt += 1
+		wb.save(self.DEentryPath.get())
+		self.DEdb.close()
+		self.textB.insert(1.0, 'DB Scheme -> Excel Complete!\n\n')
+		self.textB.insert(END, self.DEentryPath.get())
+		self.DEWindow.destroy()
+
+	def DB_SQLFunction(self) :
+		self.textB.delete(1.0, END)
+		self.cursorDS.execute("select COLUMN_NAME, DATA_TYPE, DATA_LENGTH from USER_TAB_COLUMNS where TABLE_NAME = '" + self.comboTblDS.get() +"'")
+		sqlList = self.cursorDS.fetchall()
+		self.cursorDS.execute("select COLUMN_NAME from USER_CONS_COLUMNS where CONSTRAINT_NAME = '" + self.comboTblDS.get() + "PK'")
+		pkList = self.cursorDS.fetchall()
+		self.cursorDS.execute("select SEARCH_CONDITION from ALL_CONSTRAINTS where TABLE_NAME = '" + self.comboTblDS.get() + "'")
+		nullList = self.cursorDS.fetchall()
+
+		SQLsentence = 'create table ' + self.comboTblDS.get() + ' ( '
+		SQLsentenceList = []
+		nllist = []
+		for sqllist in sqlList :
+			SQLsentenceList = SQLsentenceList + [sqllist[0] + ' ' + sqllist[1] + '(' + str(sqllist[2]) + ')']
+		for nulllist in nullList :
+			for i in range(1, len(str(nulllist[0]))) :
+				if str(nulllist[0])[i] == '"' :
+					nllist.append(str(nulllist[0])[1:i])
+		for count in range(0, len(SQLsentenceList)) :
+			for j in range(0, len(nllist)) :
+				if nllist[j] in SQLsentenceList[count] :
+					SQLsentenceList[count] = SQLsentenceList[count] + ' ' + 'NOT NULL'
+			if count == len(SQLsentenceList)-1 :
+				break
+			SQLsentenceList[count] = SQLsentenceList[count] + ','
+
+			for k in range(0, len(pkList)) :
+				if pkList[k][0] in SQLsentenceList[count] :
+					SQLsentenceList.append('CONSTRAINT ' + self.comboTblDS.get() + 'pk PRIMARY KEY (' + pkList[k][0] + ')')
+
+		for makeSentence in range(0, len(SQLsentenceList)) :
+			SQLsentence = SQLsentence + SQLsentenceList[makeSentence] + ' '
+		SQLsentence = SQLsentence + ')'
+
+		f = open(self.DSentryPath.get(), 'w')
+		f.write(SQLsentence)
+		f.close()
+		self.DSdb.close()
+		self.textB.insert(1.0, 'DB Scheme -> SQL File Complete!\n\n')
+		self.textB.insert(END, self.DSentryPath.get())
+		self.DSWindow.destroy()
